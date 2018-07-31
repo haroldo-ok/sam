a310783ba46781be3e89b7b9835a723d942b0753
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2018-07-31 19:45:25 -0300

    Corrected timing bug on sprite update routine
    This fixes #12
    
    The menu cursor was glitching because the sprites were being sent too fast
    to the VDP.
    
    Inserting a few NOPs in the correct places solved the issue.

--- lib/sprite.asm
+++ lib/sprite.asm
@@ -9,25 +9,47 @@ UpdateSprites:
     ; Sets the sprites' positions/attributes
     ;==============================================================
 	;vdp set addr (Y table)
+	
+.MACRO WAIT_VRAM
+	nop
+	nop
+	nop
+.ENDM
+
+	WAIT_VRAM
 	xor	a
 	out	($bf), a
+	WAIT_VRAM
 	ld	a, $7f
 	out	($bf), a
 	
 	; Outputs Y table
 	ld	hl, hw_sprites_y
 	ld	bc, $40BE	; 64 bytes to $be
-	otir			; Output table
+UpdateSprites_Y_Looop:
+	WAIT_VRAM
+	outi			; Output table
+	ld a, b
+	or a
+	jr nz, UpdateSprites_Y_Looop
 
 	;vdp set addr (X/Tile table)
+	WAIT_VRAM
 	ld	a, $80
 	out	($bf), a
+	WAIT_VRAM
 	ld	a, $7f
 	out	($bf), a
 	
 	; Outputs XA table
 	ld	hl, hw_sprites_xc
 	ld	bc, $80BE	; 128 bytes to $be
-	otir			; Output table
+UpdateSprites_X_Loop:	
+	WAIT_VRAM
+	outi			; Output table
+	ld a, b
+	or a
+	jr nz, UpdateSprites_X_Loop
+
     ret
 .ENDS
